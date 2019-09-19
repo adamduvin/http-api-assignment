9e626578f481aaf5d0e295d83e6f7673130031f3
Adam Duvin (RIT Student) <axd3010@ad.rit.edu>
2019-09-18 22:07:13 -0400

    Added server file

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  console.log(request.url);
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1:${port}`);
